2630f50d0e06e4a38b2625e754555a6c219f24ca
João Paulo Borges <joaopaulo.borges.gm@hotmail.com>
2021-09-23 23:45:17 -0300

    fix: removing unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,12 @@ import React, { FC } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import './App.less';
 import pt_BR from 'antd/es/locale/pt_BR';
-import ContentLayout from 'components/layout/layout';
 import Routes from 'routes';
 
 const App: FC = () => {
   return (
     <BrowserRouter>
       <ConfigProvider locale={pt_BR} />
-      {/* <ContentLayout /> */}
       <Routes/>
     </BrowserRouter>
   );
